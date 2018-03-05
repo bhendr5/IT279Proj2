86ba6eae01367067f0a37ba2a0530c1e88741bc9
Ding, Jessie <jding12@ilstu.edu>
2018-03-05 15:24:00 -0600

    Fixed bug.  Needs testing with input file

--- Shell/ShellSort.cpp
+++ Shell/ShellSort.cpp
@@ -6,14 +6,16 @@
  */
 
 // C++ implementation of Shell Sort
+#include "shellSort.h"
 #include  <iostream>
 #include <fstream>
 #include <sstream>
 #include <cmath>
+#include <vector>
 using namespace std;
  
 /* function to sort arr using shellSort */
-int shellSort(int arr[], int n)
+int shellSort(long arr[], int n)
 {
     // Find smallest exponent so that 2^exp - 1 < size, then decrement
     for (int exp = log(n+1)/log(2); exp > 0; exp--)
@@ -22,16 +24,16 @@ int shellSort(int arr[], int n)
         // The first gap elements a[0..gap-1] are already in gapped order
         // keep adding one more element until the entire array is
         // gap sorted 
-		int gap = pow(2,exp) - 1;
-        for (int i = gap; i < n; i += 1)
+		long gap = pow(2,exp) - 1;
+        for (long i = gap; i < n; i += 1)
         {
             // add a[i] to the elements that have been gap sorted
             // save a[i] in temp and make a hole at position i
-            int temp = arr[i];
+            long temp = arr[i];
  
             // shift earlier gap-sorted elements up until the correct 
             // location for a[i] is found
-            int j;            
+            long j;            
             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
                 arr[j] = arr[j - gap];
              
@@ -42,41 +44,32 @@ int shellSort(int arr[], int n)
     return 0;
 }
  
-void printArray(int arr[], int n)
-{
-    for (int i=0; i<n; i++){
-        cout << arr[i] << " ";
-	}
-}
- 
-int readfile(string inFilename, int arr[]){
+/*reads input data to a vector so we do not need to know the size in advance*/
+void readfile(char* argv[], vector<long> vect){
 	//open a file in read mode.
-	cout<<"reading from: "<<inFilename<<endl;
+	cout<<"reading list"<<endl;
 	ifstream infile;
-	infile.open(inFilename);
+	infile.open(argv[1]);
 	
-	//store data into array
-	int count = 0, i;
-	string inputLine;
-	while(!infile.eof()) {
-		getline(infile, inputLine);
-		stringstream token(inputLine);
-		while (token >> i) {
-			arr[count] = i;
-			count++;
-		}
+	//store data into vector
+	string line;
+	while (getline(infile,line)) {
+		istringstream iss(line);
+		int n;
+		iss >> n;
+		vect.push_back(n);
 	}
 	
 	//close and return
 	infile.close();
-	return count;
 }
 
-void writeFile(string outFilename, int arr[], int n) {
+/* writes sorted list to  ShellSortedList.txt*/
+void writeFile(long arr[], int n) {
 	//open a file in write mode
-	cout<<"writing to: "<<outFilename<<endl;
+	cout<<"writing to: ShellSortedList.txt"<<endl;
 	ofstream outfile;
-	outfile.open(outFilename);
+	outfile.open("ShellSortedList.txt");
 	
 	//write array to file
 	for (int i = 0; i < n; i++) {
@@ -88,20 +81,30 @@ void writeFile(string outFilename, int arr[], int n) {
 	outfile.close();
 }
 
-int main(string inFilename, string outFilename)
+/*converts the vector to an array so that it is easier to sort with random access*/
+void convert(vector<long> vect, long arr[]) {
+	for (int i = vect.size() - 1; i >= 0; i++) {
+		arr[i] = vect.back();
+		vect.pop_back();
+	}
+}
+
+int main(int argc, char*argv[])
 {
-	int n = 999999999, arr[n];	
-	int size = readfile(inFilename, arr);
+	//store data into vector
+	vector<long> vect;	
+	readfile(argv, vect);
 	
-    cout << "Array before sorting: \n";
-    printArray(arr, size);
+	//convert vector to array
+	int size = vect.size();
+	long arr[size];
+	convert(vect, arr);
  
+	//sort array
     shellSort(arr, size);
  
-    cout << "\nArray after sorting: \n";
-    printArray(arr, n);
- 
-	writeFile(outFilename, arr, size);
+    //writes sorted list to output file
+	writeFile(arr, size);
     return 0;
 }
 
---
+++ Shell/shellSort.h
@@ -0,0 +1,24 @@
+//Team Patata - Jiasui Ding, Ben Hendrix, Jacob Levin
+//IT 279
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+class mergeSort
+{
+  
+public:
+	//shellSort data
+	shellSort();
+	
+	//Stores data to vector
+	void readfile();
+	
+	//Output sorted list
+	void writefile();
+	
+	//Convert a vector into an array
+	void convert();
+};
