6aeb6a0bfc8e976bb5715b96ad506070ca230fe4
Hendrix, Ben <bhendr5@ilstu.edu>
2018-03-05 23:12:06 -0600

    Added args back to shell.

--- Insertion/InsertionSort.cpp
+++ Insertion/InsertionSort.cpp
@@ -1,83 +1,110 @@
 /*
- * InsertionSort.cpp
+ * ShellSort.cpp
  *
  *  Created on: Mar 1, 2018
  *      Author: Ben
  */
 
-#define __USE_MINGW_ANSI_STDIO 0
-#include "LongLinkedList.h"
-#include <string>
+// C++ implementation of Shell Sort
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-//Builds linkedlist from input file
-void parseFile(LongLinkedList *list, char* argv[]){
-	long num;
-	ifstream inputFile;
-	inputFile.open(argv[1]);
+/* function to sort arr using shellSort */
+int shellSort(long arr[], int n)
+{
+    // Find smallest exponent so that 2^exp - 1 < size, then decrement
+    for (int exp = log(n+1)/log(2); exp > 0; exp--)
+    {
+        // Do a gapped insertion sort for this gap size.
+        // The first gap elements a[0..gap-1] are already in gapped order
+        // keep adding one more element until the entire array is
+        // gap sorted
+		long gap = pow(2,exp) - 1;
+        for (long i = gap; i < n; i += 1)
+        {
+            // add a[i] to the elements that have been gap sorted
+            // save a[i] in temp and make a hole at position i
+            long temp = arr[i];
 
-	//Loops until no more entries in file
-	while (inputFile >> num){
-		list->addEnd(num);
-	}
-	inputFile.close();
+            // shift earlier gap-sorted elements up until the correct
+            // location for a[i] is found
+            long j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                arr[j] = arr[j - gap];
+
+            //  put temp (the original a[i]) in its correct location
+            arr[j] = temp;
+        }
+    }
+    return 0;
 }
 
-void outputFile(LongLinkedList *list,  char* argv[]){
-	ofstream outputFile(argv[2]);
-	long out = NULL;
-	LongNode *current = list->head->next;
+/*reads input data to a vector so we do not need to know the size in advance*/
+void readfile(char* filename[], vector<long> &vect, int &size){
+	//open a file in read mode.
+	cout<<"reading"<<endl;
+	ifstream infile;
+	infile.open(filename[1]);
 
-	while (current->data >> out){
-		outputFile << current->data << endl;
-		current = current->next;
-	}
-
-	outputFile.close();
-}
-
-
-
-//sorts linkedlist using insertion sort
-void sortList(LongLinkedList *list, char* argv[]){
-
-	//selects first node after null head
-	LongNode *inputNode = list->head->next;
-	LongLinkedList *sortedList = new LongLinkedList();
-
-	// Loop to go through original linked list from file until null tail is encountered
-	while(inputNode->next != nullptr){
-		LongNode *prevNode  = sortedList->tail->prev;
-
-		//adds data to end if condition is already met
-		if(inputNode->data >= prevNode->data){
-			sortedList->addEnd(inputNode->data);
-		}
-
-
-		else{
-			while(inputNode->data <= prevNode->data){
-				prevNode = prevNode->prev;
-			}
-			sortedList->addBefore(inputNode->data, prevNode);
-		}
-
-		//switches to next node in inputList
-		inputNode = inputNode->next;
+	//store data into vector
+	string line;
+	while (getline(infile,line)) {
+		istringstream iss(line);
+		int n;
+		iss >> n;
+		vect.push_back(n);
 	}
 
-	outputFile(sortedList, argv);
+	size = vect.size();
+
+	//close and return
+	infile.close();
 }
 
+/* writes sorted list to  ShellSortedList.txt*/
+void writeFile(char* filename[], long arr[], int n) {
+	//open a file in write mode
+	cout<<"writing"<<endl;
+	ofstream outfile;
+	outfile.open(filename[2]);
 
+	//write array to file
+	for (int i = 0; i < n; i++) {
+		outfile << arr[i] << endl;
+	}
 
-int main(int argc, char* argv[]){
-	LongLinkedList *list= new LongLinkedList();
-	parseFile(list,argv);
-
-	sortList(list,argv);
-
-	return 0;
+	//finish and close
+	outfile.close();
 }
+
+/*converts the vector to an array so that it is easier to sort with random access*/
+void convert(vector<long> vect, long arr[], int size) {
+	for (int i = size - 1; i >= 0; i--) {
+		arr[i] = vect.back();
+		vect.pop_back();
+	}
+}
+
+int main(int argc, char *filename[])
+{
+	//store data into vector
+	vector<long> vect;
+	int size = 0;
+	readfile(filename, vect, size);
+
+	//convert vector to array
+	long arr[size];
+	convert(vect, arr, size);
+
+	//sort array
+    shellSort(arr, size);
+
+    //writes sorted list to output file
+	writeFile(filename, arr, size);
+    return 0;
+}
+
